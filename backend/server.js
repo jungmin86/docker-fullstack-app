8229b9875052ee1602ef13667f7753c719568b96
Jungmin Kim <jmkimm00@icloud.com>
2023-03-01 12:04:46 +0900

    setting docker compose(wootcamp)

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,13 @@ const app = express();
 app.use(bodyParser.json());
 
 // lists 테이블 생성하기
-db.pool.query(`CREATE TABLE lists (
-    id INTEGER AUTO_INCREMENT,
-    value TEXT,
-    PRIMARY KEY (id)
-)`, (err, results, fields) => {
-    console.log('results', results);
-})
+// db.pool.query(`CREATE TABLE lists (
+//     id INTEGER AUTO_INCREMENT,
+//     value TEXT,
+//     PRIMARY KEY (id)
+// )`, (err, results, fields) => {
+//     console.log('results', results);
+// })
 
 //DB lists 테이블에 있는 모든 데이터를 프론트 서버에 보내주기
 app.get('/api/values', function(req, res) {

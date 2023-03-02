43f752e0425eb242c8c7940d31c7e5c3bc78d7e9
Jungmin Kim <jmkimm00@icloud.com>
2023-03-03 00:12:42 +0900

    fixed

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ const app = express();
 app.use(bodyParser.json());
 
 // lists 테이블 생성하기
-// db.pool.query(`CREATE TABLE lists (
-//     id INTEGER AUTO_INCREMENT,
-//     value TEXT,
-//     PRIMARY KEY (id)
-// )`, (err, results, fields) => {
-//     if(err) console.log("err");
-//     console.log('results11', results);
-// })
+db.pool.query(`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+)`, (err, results, fields) => {
+    if(err) console.log("err");
+    console.log('results11', results);
+})
 
 //DB lists 테이블에 있는 모든 데이터를 프론트 서버에 보내주기
 app.get('/api/values', function(req, res) {
